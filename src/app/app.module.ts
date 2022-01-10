f4391f1e2fd90582c379ed9a13dc9dc9fd9db95e
¨HermesGabriel18¨ <hermesgabriel95@gmail.com>
2022-01-10 08:19:51 -0400

    :hammer: Library installed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { NgxEchartsModule } from 'ngx-echarts';
+
+import { PieChartComponent } from './pie-chart/pie-chart.component';
+
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent, PieChartComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgxEchartsModule.forRoot({
+      echarts: () => import('echarts'),
+    }),
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
